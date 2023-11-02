c61de4be041bc077ef36815282fe51e875993625
0xR3TR0 <oliwier@klefas.com>
2023-11-02 23:05:52 +0100

    0xR3TRO

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-// Scripts and functions on this page are the creation of 0xR3TR0. All rights reserved.
+// Scripts and functions on this page are the creation of 0xR3TR0. All rights reserved. 
 const chatInput = document.querySelector(".chat-input textarea");
 const sendChatBtn = document.querySelector(".chat-input span");
 const chatbox = document.querySelector(".chatbox");
